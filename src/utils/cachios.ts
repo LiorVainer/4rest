ee4e2fc2cdbab64dc4df6ca9f2b9b4b7e0dfcaca
Lior Vainer <lior.vainer@agamimp.net>
2022-02-06 10:45:39 +0200

    Fix(Cachios Support): Downgraded to last axios version compatible with cachios

--- src/utils/cachios.ts
+++ src/utils/cachios.ts
@@ -2,4 +2,6 @@ import cachios from "cachios";
 import { FetchInstance } from "types/fetchInstance";
 
 export const fetchInstanceToCachiosInstance = (fetchInstance: FetchInstance) =>
-  "axiosInstance" in fetchInstance ? fetchInstance : cachios.create(fetchInstance);
+  "axiosInstance" in fetchInstance
+    ? fetchInstance
+    : cachios.create(fetchInstance);
