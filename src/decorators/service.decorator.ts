dec837b08a1526180126a4289a1fdd7144dae446
Lior Vainer <liorvainer@gmail.com>
2022-02-17 16:31:25 +0200

    feat(decorator): enabled support for decorators in project

--- src/decorators/service.decorator.ts
+++ src/decorators/service.decorator.ts
@@ -1,19 +1,24 @@
-import { ArpeggiosInstance, ArpeggiosService, ServiceConfig, ServiceMethods } from "index";
+import { ArpeggiosInstance, ArpeggiosService, ServiceConfig, ServiceMethods } from "../index";
 import { ObjectId } from "mongodb";
 import { createRequestMethods } from "../RequestFactory";
 
-// export function Service<Response, Payload = Response, IdType = ObjectId>(instance: ArpeggiosInstance, prefix: string) {
-//   // this is the decorator factory, it sets up
-//   // the returned decorator function
-//   return function <T extends { new (...args: any[]): ArpeggiosService<Response, Payload, IdType> }>(
-//     originalConstructor: T
-//   ) {
-//     return class extends originalConstructor {
-//       protected methods: ServiceMethods;
-//       constructor(...args: any[]) {
-//         super(...args);
-//         this.methods = createRequestMethods(prefix, instance);
-//       }
-//     };
-//   };
-// }
+export function Service<Response = any, Payload = Response, IdType = ObjectId>(
+  instance: ArpeggiosInstance,
+  prefix: string
+) {
+  // this is the decorator factory, it sets up
+  // the returned decorator function
+  return function <T extends { new (...args: any[]): {} }>(originalConstructor: T) {
+    console.log("proto1", originalConstructor.prototype);
+
+    Object.setPrototypeOf(originalConstructor.prototype, Object.getPrototypeOf(ArpeggiosService));
+    console.log("proto2", originalConstructor.prototype);
+
+    return class extends originalConstructor {
+      methods = createRequestMethods(prefix, instance);
+      getAll() {
+        return this.methods.get<Response[]>();
+      }
+    };
+  };
+}
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { ArpeggiosStatic } from "./arpeggios/static";
 
 export { ArpeggiosInstance } from "./arpeggios/instance";
 export { ServiceMethods } from "./types/arpeggios";
+export { Service } from "./decorators/service.decorator";
 export { InstanceConfig, ArpeggiosStatic } from "./arpeggios/static";
 export { ArpeggiosService, ServiceConfig } from "./arpeggios/service";
 
