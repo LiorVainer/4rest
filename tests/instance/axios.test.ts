f1c0931b4beec3bdc0c5354ad00023be8ec9cefc
Lior Vainer <liorvainer@gmail.com>
2022-04-20 22:21:04 +0300

    test(AxiosInstance): added tests to see if axios instance inside forestService is fully function AxiosInstance

--- tests/instance/axios.test.ts
+++ tests/instance/axios.test.ts
@@ -7,3 +7,8 @@ const forestInstance: ForestInstance = forest.create(axios);
 test("Forest Instance Defined", () => {
   expect(forestInstance).toBeDefined();
 });
+
+test("Axios Instance Defined", () => {
+  expect(forestInstance.axiosInstance).toBeDefined();
+  expect(forestInstance.axiosInstance.get).toBeDefined();
+});
--- tests/instance/axiosConfig.test.ts
+++ tests/instance/axiosConfig.test.ts
@@ -2,8 +2,16 @@ import forest, { ForestInstance } from "../../src";
 
 export const forestInstance: ForestInstance = forest.create({
   headers: { "X-Custom-Header": "Custom Value" },
+  baseURL: "http://localhost:5000",
 });
 
 test("Forest Instance Defined", () => {
   expect(forestInstance).toBeDefined();
+});
+
+test("Axios Instance Defined", () => {
+  expect(forestInstance.axiosInstance).toBeDefined();
+  expect(forestInstance.axiosInstance.get).toBeDefined();
+  expect(forestInstance.axiosInstance.defaults.baseURL).toBeDefined();
+  expect(forestInstance.axiosInstance.defaults.headers).toBeDefined();
 });
