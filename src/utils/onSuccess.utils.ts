d67c022cff96f9c2d31eff05c15603839cb83049
Lior Vainer <liorvainer@gmail.com>
2022-04-30 04:00:09 +0300

    fix(onSuccess.utils): added docs on function

--- src/utils/onSuccess.utils.ts
+++ src/utils/onSuccess.utils.ts
@@ -6,6 +6,12 @@ import {
 } from "../constants/onSuccess.const";
 import { Metadata } from "../types/onSuccess";
 
+/**
+ *
+ * @param res - Axios Response to be handled
+ * @param metadata - Metadata of the request and service config
+ * @returns - response handle function
+ */
 export const onSuccessHandle = <T>(res: AxiosResponse<T>, metadata?: Metadata) => {
   if (metadata?.serviceConfig) {
     if (metadata.serviceConfig.onSuccess && !metadata.serviceConfig.validation) {
