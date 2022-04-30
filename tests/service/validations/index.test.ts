ff36828df6faee4ec72c5189f4372b8fc01f1401
Lior Vainer <liorvainer@gmail.com>
2022-04-30 04:02:22 +0300

    test(validation): added some basic test to check if validation config for services works

---
+++ tests/service/validations/index.test.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+import { User, UserWithIdSchema, UserWithId, UserSchema } from "../../types/user";
+import forest, { ForestInstance, ForestService } from "../../../src";
+import { z } from "zod";
+
+export let forestInstance: ForestInstance;
+export let userService: ForestService<UserWithId, User, number>;
+
+beforeAll(() => {
+  forestInstance = forest.create();
+  userService = forestInstance.createService<UserWithId, User, number>("user", {
+    validation: {
+      onMethods: {
+        post: { types: { requestPayload: UserSchema, resoponseData: UserWithIdSchema } },
+        getById: { types: { resoponseData: UserSchema.strict() } },
+      },
+      types: { resoponseData: UserWithIdSchema },
+    },
+  });
+});
+
+describe("Forest Classes", () => {
+  test("Forest Instance Defined", () => {
+    expect(forestInstance).toBeDefined();
+  });
+
+  test("Forest Service Defined", () => {
+    expect(userService).toBeDefined();
+  });
+});
