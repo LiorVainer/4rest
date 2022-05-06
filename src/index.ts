45aa462f8176135603d66c43b22d40b7ff7d4a8b
Lior Vainer <liorvainer@gmail.com>
2022-05-06 19:47:25 +0300

    fix(index.ts): updated types exports of package

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,11 @@ export { InstanceConfig as ForestInstanceConfig } from "./classes/instance.class
 export { ForestInstance } from "./classes/instance.class";
 export { ServiceMethodsCreator as ForestServiceMethodsCreator } from "./types/service.types";
 export { ForestStatic } from "./classes/static.class";
+export { Metadata } from "./types/metadata.types";
+export { OnErrorFunction } from "./types/onError.types";
+export { OnSuccessFunction } from "./types/onSuccess.types";
+export { GlobalServiceConfig, ServiceFunction, ServiceMethodsCreator } from "./types/service.types";
+export { MethodValidationConfig, ServiceValidationConfig } from "./types/validation.types";
 
 export { ForestService } from "./classes/service.class";
 export { ServiceConfig as ForestServiceConfig } from "./types/service.types";
